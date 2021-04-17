f83999ef8e7dd78c7d48f1f0c35c6c1ffb8d1579
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-17 16:42:55 +0100

    issue done. closes #2.

---
+++ GenericsParts2/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        FootballPlayer pat = new FootballPlayer("Joe");
+        SoccerPlayer beckham  = new SoccerPlayer("Beckham");
+
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numberOfPlayers());
+    }
+}
